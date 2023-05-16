7f080011116bb1b37cda2307e6049f6bae62ab2c
lese93 <lee_se93@naver.com>
2023-05-16 22:01:23 +0900

    settingUI

--- app/src/main/java/com/example/gptchatbot/MainActivity.kt
+++ app/src/main/java/com/example/gptchatbot/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.gptchatbot
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gptchatbot.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        binding.settingBtn.setOnClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
